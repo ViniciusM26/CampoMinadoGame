f587e4ed5591ec7a20870f93acbb992747cf2e06
ViniciusM26 <vinicius.m2602@gmail.com>
2024-01-31 15:08:06 -0300

    Dificuldades nos modos de jogo
    Implementação inicial das respectivas dificuldades do jogo (Fácil, Médio e Difícil) nos modos Solo e Multiplayer.
    Ver a respeito do modo Maluco

--- src/campoMinado/CampoMinado.java
+++ src/campoMinado/CampoMinado.java
@@ -8,13 +8,16 @@ import campoMinado.ModosJogo.JogoMultiplayer;
 import campoMinado.ModosJogo.JogoSolo;
 import campoMinado.Tabuleiros.Tabuleiro;
 import campoMinado.Tabuleiros.TabuleiroMaluco;
+import campoMinado.Tabuleiros.TabuleiroFacil;
+import campoMinado.Tabuleiros.TabuleiroMedio;
+import campoMinado.Tabuleiros.TabuleiroDificil;
 
 public class CampoMinado {
 
 	public static void main(String[] args) {
 		// criar os jogadores
 		Jogo jogo; // função que cria o objeto jogo multiplayer
-		int modo;
+		int modo, dificuldade;
 		Tabuleiro tabuleiro;
 
 		try {
@@ -26,16 +29,65 @@ public class CampoMinado {
 		
 		switch (modo) {
 			case 1:
-				tabuleiro = new Tabuleiro(7,7); // cria o tabuleiro
-				jogo = new JogoSolo(tabuleiro);
-				jogo.Jogar(); // função que joga
+				try {
+					dificuldade = Menu.rodarMenuDificuldade(); // Abre o menu de dificuldade
+				} catch (InputMismatchException e) {
+					System.out.println("Erro: " + e.getMessage());
+					return;
+				}
+
+				switch (dificuldade) {
+					case 1:
+						tabuleiro = new TabuleiroFacil(); 
+						jogo = new JogoSolo(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					case 2:
+						tabuleiro = new TabuleiroMedio(); 
+						jogo = new JogoSolo(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					case 3:
+						tabuleiro = new TabuleiroDificil(); 
+						jogo = new JogoSolo(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					default:
+						System.out.println("Valor inválido");
+						break;
+				}
 				break;
-			
+
 			case 2:
-				tabuleiro = new Tabuleiro(7, 7);// cria o tabuleiro
-				jogo = new JogoMultiplayer(tabuleiro);
-				jogo.Jogar(); // função que joga
+				try {
+					dificuldade = Menu.rodarMenuDificuldade(); // Abre o menu de dificuldade
+				} catch (InputMismatchException e) {
+					System.out.println("Erro: " + e.getMessage());
+					return;
+				}
+
+				switch (dificuldade) {
+					case 1:
+						tabuleiro = new TabuleiroFacil(); 
+						jogo = new JogoMultiplayer(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					case 2:
+						tabuleiro = new TabuleiroMedio(); 
+						jogo = new JogoMultiplayer(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					case 3:
+						tabuleiro = new TabuleiroDificil(); 
+						jogo = new JogoMultiplayer(tabuleiro);
+						jogo.Jogar(); // função que joga
+						break;
+					default:
+						System.out.println("Valor inválido");
+						break;
+				}
 				break;
+
 			case 3:
 				tabuleiro = new TabuleiroMaluco(7, 7); // cria o tabuleiro
 				jogo = new JogoMaluco(tabuleiro);
--- src/campoMinado/Menu.java
+++ src/campoMinado/Menu.java
@@ -7,17 +7,17 @@ public class Menu {
 
     public static int rodarMenu(){
 
-        System.out.println("Olá, escolha o modo de jogo:\n1.Solo\n2.Multijogador\n3.Maluco"); //imprime as opções na tela
+        System.out.println("Olá, escolha o modo de jogo:"); //imprime as opções na tela
+        System.out.println("1 - Iniciar um jogo Solo");
+        System.out.println("2 - Iniciar um jogo Multiplayer");
+        System.out.println("3 - Iniciar um jogo Maluco");
+        System.out.println("4 - Sair");
         Scanner scanner = new Scanner(System.in);
-
-        // tenta fazer a entrada
 
         try{
             int modo = scanner.nextInt();
             return modo;
         }
-
-        // se der erro avisa o usuario e acaba o programa
 
         catch (InputMismatchException e ) {
             	System.out.println("Erro: Certifique-se de digitar um valor inteiro válido.");
@@ -25,6 +25,28 @@ public class Menu {
                 return -1;
 			}
 
+    }
+
+    public static int rodarMenuDificuldade() {
+    
+        System.out.println("Escolha a dificuldade:"); //imprime as opções na tela
+        System.out.println("1 - Fácil");
+        System.out.println("2 - Médio");
+        System.out.println("3 - Difícil");
+        System.out.print  ("4 - Sair");
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+    
+        try {
+            int dificuldade = scanner.nextInt();
+            return dificuldade;
+        } 
+        
+        catch (InputMismatchException e) {
+                System.out.println("Erro: Certifique-se de digitar um valor inteiro válido.");
+                scanner.nextLine();
+                return -1;
+            }
     }
 
 }
