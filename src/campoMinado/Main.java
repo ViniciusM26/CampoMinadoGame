1ab0c76c97395de26f387c61c3b6e4a2d012f8db
ViniciusM26 <vinicius.m2602@gmail.com>
2024-02-24 11:10:12 -0300

    Pequena atualização do HIstorico
    Testes feitos de forma manual, tendo que alterar o método setPontos para public provisoriamente para realizar os teste, mas está ordenando de forma correta com as maiores pontuações.

--- src/campoMinado/Main.java
+++ src/campoMinado/Main.java
@@ -2,12 +2,16 @@ package campoMinado;
 
 import javax.swing.JFrame;
 import campoMinado.MinhaGUI.MinhaGUI;
+import campoMinado.ModosJogo.Jogadores.Historico;
 
 public class Main extends JFrame {
     
     public static void main(String[] args){
         CampoMinado campoMinado = new CampoMinado();
-        //campoMinado.jogar();
+        // campoMinado.jogar();
+        
+        Historico historico = new Historico();
+	    
         MinhaGUI minhaGUI = new MinhaGUI();
         minhaGUI.initGUI();
     }
--- src/campoMinado/ModosJogo/Jogadores/Historico.java
+++ src/campoMinado/ModosJogo/Jogadores/Historico.java
@@ -8,17 +8,25 @@ public class Historico {
 	
     private ArrayList<Jogador> jogadores;
 
-    public Historico(){
-        jogadores = new ArrayList<Jogador>();
+    public Historico() {
+        jogadores = new ArrayList<>();
     }
 
     public void adicionarJogador(Jogador jogador) {
         jogadores.add(jogador);
-        Collections.sort(jogadores, new Comparator<Jogador>(){
+        Collections.sort(jogadores, new Comparator<Jogador>() {
             @Override
             public int compare(Jogador j1, Jogador j2) {
                 return Integer.compare(j2.getPontos(), j1.getPontos());
             }
         });
+    }
+
+    public void imprimir() {
+        System.out.println("Histórico de Jogadores:");
+        for (int i = 0; i < jogadores.size(); i++) {
+            System.out.printf("%d. %s", i + 1, jogadores.get(i).getNome());
+            System.out.printf(" - Pontuação: %d\n", jogadores.get(i).getPontos());
+        }
     }
 }
